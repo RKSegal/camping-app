de9c735c9ccd2f3631cb8e0cec08d20d55a18fdc
Rachel <rachel.k.segal@gmail.com>
2018-04-24 20:50:40 -0700

    added time since created

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"));
 app.use(flash());
 
+app.locals,moment = require("moment");
+
 //Passport Configuration
 app.use(require("express-session")({
     secret: "Dogs dogs dogs",
